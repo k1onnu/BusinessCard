a6dfc2058d1cace71f4ba98d65d450a03887de4c
k1 <tichdies@gmail.com>
2023-03-21 10:26:25 +0300

    add BusinessCard.

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("**************************\n");
+        System.out.print("****\t  Визитка\t  ****\n");
+        System.out.print("**** Name: \"you name\" ****\n");
+        System.out.print("**** Skype:   you skype ****\n");
+        System.out.print("**************************");
+    }
+}
